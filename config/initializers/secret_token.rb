0c156ad697aee7b555ae6c670a1156fbdef37c04
Ashish Bista <poisonta03@gmail.com>
2012-01-03 09:29:32 +0545

    rails initial with rspec installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pickapic::Application.config.secret_token = '056efd5cbe2b05e041deaa94f776189bc650c618740db67372c05b193a966a354dff29f6cf47956676e2f41c7a67963e7edb464a23eddb2b70e19468dc774a6a'
